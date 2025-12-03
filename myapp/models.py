796087a91ab37828bd5098ff608f3680922b5074
Satyajeet Yadav <syadav@metaapply.io>
2025-12-03 15:24:14 +0530

    model change

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)  # Title of the book
+    author = models.CharField(max_length=100)  # Author of the book
+    publish_date = models.DateField()  # Publish date of the book
+    isbn = models.CharField(max_length=13, unique=True)  # ISBN number (unique)
+
+    def __str__(self):
+        return self.title  # Display title when we print the object

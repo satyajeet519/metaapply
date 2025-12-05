eef9ac7e5180c6b5b77386fc16aa9a8d653ef997
Ekta Gupta <ektaguptaa858@gmail.com>
2025-12-05 12:38:05 +0530

    Testing Phase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
         <h1>Test</h1>
-=======
-      <header className="App-header">
-        <h1>satyajeet yadav</h1>
-      </header>
->>>>>>> 88a36e278709b93f3019914e5d0792d7659b6817
     </div>
   );
 }

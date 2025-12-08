a68d7058375c35746cf5cb4b20b234b56f9174b2
Satyajeet Yadav <syadav@metaapply.io>
2025-12-08 13:26:03 +0530

    name addedxdd

--- student/serializers.py
+++ student/serializers.py
@@ -5,5 +5,6 @@ from .models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ('id', 'user', 'phone', 'city', 'created_at')
+        fields = ('id', 'user', 'phone', 'city',
+                  'email', 'username', 'created_at')
         read_only_fields = ('id', 'created_at', 'user')

e4614ca457004efdcf8510f191824802fcd41404
Satyajeet Yadav <syadav@metaapply.io>
2025-12-08 13:29:49 +0530

    name addedxddd

--- student/admin.py
+++ student/admin.py
@@ -4,5 +4,5 @@ from .models import Student
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'phone', 'city', 'created_at')
+    list_display = ('user', 'phone', 'city', 'email', 'username', 'created_at')
     search_fields = ('user__username', 'phone', 'city')
